69810c144e90d599f100435898fdb1cb4be197f1
Mickhal <mbilliotte@gmail.com>
2021-12-14 08:45:18 +0100

    End of section 17

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -7,6 +7,7 @@ import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
 import { BreadcrumbModule } from 'xng-breadcrumb';
 import { SectionHeaderComponent } from './section-header/section-header.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -19,7 +20,8 @@ import { SectionHeaderComponent } from './section-header/section-header.componen
       positionClass: 'toast-bottom-right',
       preventDuplicates: true,
     }),
-    BreadcrumbModule
+    BreadcrumbModule,
+    SharedModule
   ],
   exports: [
     NavBarComponent,
---
+++ client/src/app/shared/models/adress.ts
@@ -0,0 +1,8 @@
+export interface IAdddress {
+    firstName: string;
+    lastName: string;
+    street: string;
+    city: string;
+    state: string;
+    zipCode: string;
+}
